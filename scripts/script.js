0be4e5b01747c3cec6eba02d3bd43a5501683607
Amanita19 <51726798+Amanita19@users.noreply.github.com>
2021-05-14 18:36:06 -0700

    initial pass

--- scripts/script.js
+++ scripts/script.js
@@ -4,15 +4,49 @@ import { router } from './router.js'; // Router imported so you can use it to ma
 const setState = router.setState;
 
 // Make sure you register your service worker here too
-
-document.addEventListener('DOMContentLoaded', () => {
-  fetch('https://cse110lab6.herokuapp.com/entries')
-    .then(response => response.json())
-    .then(entries => {
-      entries.forEach(entry => {
-        let newPost = document.createElement('journal-entry');
-        newPost.entry = entry;
-        document.querySelector('main').appendChild(newPost);
-      });
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/sw.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
     });
+}
+
+window.addEventListener("popstate", (event) => {
+    setState(event.state, true);
+});
+
+let settingsGear = document.querySelector("[alt=settings]");
+settingsGear.onclick = () => {
+    setState({ name: "settings" }, false)
+};
+
+
+let header = document.getElementsByTagName("h1")[0];
+header.addEventListener('click', () => {
+    setState({ name: "home" }, false);
+});
+
+
+//journal entries
+document.addEventListener('DOMContentLoaded', () => {
+    let id = 0;
+    fetch('https://cse110lab6.herokuapp.com/entries')
+        .then(response => response.json())
+        .then(entries => {
+            entries.forEach(entry => {
+                let newPost = document.createElement('journal-entry');
+                newPost.entry = entry;
+                ++id;
+                newPost.id = id;
+                newPost.onclick = () => {
+                    setState({ name: "entry", id: newPost.id }, entry);
+                };
+                document.querySelector('main').appendChild(newPost);
+            });
+        });
 });
